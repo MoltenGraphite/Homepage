e783394ab5f20f9f30f7461aa6302398df249c1b
John Kayne <johnhoganuke2@gmail.com>
2018-01-31 14:50:47 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+var projects = {
+  "JHRand":(urb){
+    
+  }
+};
+
+var urb = Object;
+urb.prototype.toString = function() {
+  return "<div><h4>"+this.title+"</h4><p>"+this.description+"</p></div>";
+}
+
