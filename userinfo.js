4c9af5aabe292de7f4d3f97b06dc74558ae7615d
John Kayne <johnhoganuke2@gmail.com>
2018-02-02 14:40:23 -0500

    Create userinfo.js

---
+++ userinfo.js
@@ -0,0 +1,15 @@
+import "userbase";
+
+function getParam(c) {
+  var url_string = window.location.href;
+  var url = new URL(url_string);
+  return url.searchParams.get(c);
+}
+
+window.onload = function() {
+  var username = getParam("user");
+  var user = userbase[username];
+  var knx = document.getElementById;
+  knx("username").innerText=user.name;
+  knx("email").innerText=user.email;
+}
